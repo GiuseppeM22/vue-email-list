737834cca94b155b78788d58c7d6766a99b267d9
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-05-17 17:51:33 +0200

    add : email btn generetor

--- js/script.js
+++ js/script.js
@@ -7,14 +7,18 @@ createApp({
       emails : []
     }
   },
+  methods : {
+  emailGen(){
+    for (let i = 0; i < 10; i++) {
+        // const rispEle = array[i];
+        axios.get( this.email ).then((risp) => {
+           const risposta = risp.data.response
+           this.emails.push(risposta)
+         }); 
+    }
+  }},
   mounted() {
-for (let i = 0; i < 10; i++) {
-    // const rispEle = array[i];
-    axios.get( this.email ).then((risp) => {
-       const risposta = risp.data.response
-       this.emails.push(risposta)
-     }); 
-}
+
 
 }
 }).mount('#app')
